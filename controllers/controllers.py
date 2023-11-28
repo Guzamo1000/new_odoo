6c0916db5482b0c1f1e36f58a7e08a0ac9a62fe0
guzamo1000 <guzamo60@gmail.com>
2023-11-28 17:06:16 +0700

    done

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -16,3 +16,9 @@ class OtManagement(http.Controller):
             'ot':ot,
             "url": url
         })
+    @http.route("/ot_management/ot_registration_detail/<int:id>", website=True, auth="public")
+    def ot_registration_detail(self,id):
+        action_ot_registration_id=request.env.ref("ot_management.ot_registration_request_action")
+        menu_ot_registration_id=request.env.ref("ot_management.ot_request")
+        url=f"/web?#id={id}&action={action_ot_registration_id.id}&model=ot.registration&view_type=form&menu_id={menu_ot_registration_id.id}"
+        return redirect(url)
